dfd5eeb8926359dc44ad5aee78607921b594339c
sourya <sourya.kaushik@simpplr.com>
2023-10-25 09:30:20 +0530

    Error handling setup

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,34 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import { HttpError } from 'http-errors';
+
+import logger from './config/logger';
 
 const app = express();
 
 app.get('./', (req, res) => {
   res.send('welcome from auth');
+});
+
+/*
+    Whenever there is any error inside any route, it get caught in global express error handler.
+    If there is async function then any error inside that function will not caught by gloabl express
+    middleware, so use GEEH we use next(error) inside async funtion inside any route
+*/
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+  logger.error(err.message);
+  const statusCode = err.statusCode || 500;
+
+  res.status(statusCode).json({
+    errors: [
+      {
+        type: err.name,
+        msg: err.message,
+        path: '',
+        location: '',
+      },
+    ],
+  });
 });
 
 export default app;
