58641eda7316378989ae89235ea008c40a6c5a06
sourya <sourya.kaushik@simpplr.com>
2023-10-27 10:31:32 +0530

    add logger for auth controller

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,13 +1,34 @@
-import { Response } from 'express';
+import { NextFunction, Response } from 'express';
+import { Logger } from 'winston';
 import { UserService } from '../services/userService';
 import { ResgisterUserRequest } from '../types';
 
 export class AuthController {
-  constructor(private userService: UserService) {}
-  async register(req: ResgisterUserRequest, res: Response) {
+  constructor(
+    private userService: UserService,
+    private logger: Logger,
+  ) {}
+  async register(req: ResgisterUserRequest, res: Response, next: NextFunction) {
     const { firstName, lastName, email, password } = req.body;
+    this.logger.debug('New request to register a user', {
+      firstName,
+      lastName,
+      email,
+    });
+    try {
+      const user = await this.userService.create({
+        firstName,
+        lastName,
+        email,
+        password,
+      });
 
-    await this.userService.create({ firstName, lastName, email, password });
+      this.logger.info('User has been register', { id: user.id });
+      res.status(201).json({ id: user.id });
+    } catch (err) {
+      next(err);
+      return;
+    }
 
     res.status(201).json();
   }
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -4,13 +4,16 @@ import { AuthController } from '../controllers/AuthController';
 import { UserService } from '../services/userService';
 import { AppDataSource } from '../config/data-source';
 import { User } from '../entity/User';
+import logger from '../config/logger';
 
 const router = express.Router();
 const userRepository = AppDataSource.getRepository(User);
 const userService = new UserService(userRepository);
-const authController = new AuthController(userService); // dependency injection
+const authController = new AuthController(userService, logger); // dependency injection
 
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
-router.post('/register', (req, res) => authController.register(req, res));
+router.post('/register', (req, res, next) =>
+  authController.register(req, res, next),
+);
 
 export default router;
