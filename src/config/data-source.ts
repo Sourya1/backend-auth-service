b6e3a0a6983b6fdd9091c93b6dab3bd4d51c20f2
sourya <sourya.kaushik@simpplr.com>
2023-11-06 10:24:09 +0530

    added migration script and files

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -12,9 +12,9 @@ export const AppDataSource = new DataSource({
   password: Config.DB_PASSWORD,
   database: Config.DB_NAME,
   // Don't use in production, also make it false
-  synchronize: true,
+  synchronize: false,
   logging: false,
   entities: [User, RefreshToken], // tables info
-  migrations: [],
+  migrations: ['src/migration/*.ts'],
   subscribers: [],
 });
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,9 @@
 import { config } from 'dotenv';
 import path from 'path';
 
-config({ path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`) }); // env.dev or env.test
+config({
+  path: path.join(__dirname, `../../.env.${process.env.NODE_ENV || 'dev'}`),
+}); // env.dev or env.test
 
 const {
   PORT,
